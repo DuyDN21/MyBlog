87fe29677ba55042649d3169518fa4e049d606d6
duydn2 <dongocduy.duydo@gmail.com>
2025-10-10 23:39:11 +0700

    Add authen

---
+++ MyBlog/MyBlog.App/Controllers/AuthenController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyBlog.App.Controllers
+{
+    public class AuthenController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
