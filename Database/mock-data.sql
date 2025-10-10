272274ee0c0a64d844d5baf859738d06bf41b0c2
duydn2 <dongocduy.duydo@gmail.com>
2025-10-10 23:23:16 +0700

    Create mock-data.sql

---
+++ Database/mock-data.sql
@@ -0,0 +1,6 @@
+INSERT INTO personal_blog.users (username, email, `password`, role, status)
+VALUES
+('user1', 'sudeptrainhuduy@gmail.com', '123456', 'user', 'active'),
+('user2', 'duydockerhub@gmail.com', '123456', 'user', 'active'),
+('user3', 'forestfakethaolam@gmail.com', '123456', 'user', 'active'),
+('admin1', 'dongocduy.duydo@gmail.com', 'admin123', 'admin', 'active');
